8221d5e5ad2ffc2298af31664eb8a93bfe69948e
FERNANDO <elderarias2015@gmail.com>
2022-03-17 16:31:56 -0300

    teams-11

--- Programming building blocks/teams-11.py
+++ Programming building blocks/teams-11.py
@@ -1,40 +1,20 @@
-names = []
-balances = []
+names=[]
+titles=[]
+ids=[]
+salaries=[]
+with open('hr_system.txt') as lines:
+    for line in lines:
+        line=line.strip()
+        line=line.split()
+        names.append(line[0])
+        titles.append(line[2])
+        ids.append(line[1])
+        salaries.append(line[3])
 
-print('Enter the names and balances of bank accounts (type: quit when done)')
-account_name=input('What is the name of this account? ')
 
-while account_name.lower()!='quit':
-    names.append(account_name)
-    account_balance=float(input('What is the balance? '))
-    balances.append(account_balance)
-    account_name=input('What is the name of this account? ')
 
-print('\nAccount information: ')
 for i in range(len(names)):
-    print(f'{i+1}. {names[i]} - ${balances[i]:.2f}')
-
-highest=max(balances)
-index=balances.index(highest)
-name_highest=names[index]
-
-print(f'\nTotal: ${sum(balances)}')
-print(f'Avarage: ${sum(balances)/len(balances):.2f}')
-print(f'Highest balance: {name_highest} - ${highest}')
-
-
-
-update_account=input('\nDo you want to update an account? ')
-while update_account.lower()=='yes':
-    index_update=int(input('What account index do you want to update? '))
-    new_amount=float(input('What is the new amount? '))
-    balances[index_update-1]=new_amount
-    print('\nAccount information: ')
-    for i in range(len(names)):
-        print(f'{i+1}. {names[i]} - ${balances[i]:.2f}')
-    
-    update_account=input('\nDo you want to update an account? ')
-
-print('\nAccount information: ')
-for i in range(len(names)):
-    print(f'{i+1}. {names[i]} - ${balances[i]:.2f}')
+    paycheck=float(salaries[i])/24
+    if titles[i]=='Engineer':
+        paycheck+=1000
+    print(f'{names[i]} (ID: {ids[i]}), {titles[i]} - ${paycheck:.2f}')
