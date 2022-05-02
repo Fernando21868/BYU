b08f15106ce9afb5199e8050aa0b58e592d55fa2
FERNANDO <elderarias2015@gmail.com>
2022-05-02 13:23:34 -0300

    scene terminado creo

--- cse111/L03/scene.py
+++ cse111/L03/scene.py
@@ -19,7 +19,6 @@ def main():
     # as draw_sky and draw_ground here.
     draw_sky(canvas, scene_width, scene_height)
     draw_ground(canvas, scene_width, scene_height)
-    draw_grid(canvas, scene_width, scene_height, 50)
     # Call the finish_drawing function
     # in the draw2d.py library.
     finish_drawing(canvas)
@@ -31,18 +30,12 @@ def draw_sky(canvas, scene_width, scene_height):
     """Draw the sky and all the objects in the sky."""
     draw_rectangle(canvas, 0, scene_height / 3,
                    scene_width, scene_height, width=0, fill="midnightBlue")
+    draw_stars(canvas)
+    draw_clouds(canvas)
+    draw_moon(canvas)
 
-    draw_polygon(canvas, 150, 150, 200, 100, 150, 50, 100, 100, fill='black')
 
-    for i in range(20):
-        random_clouds = random.random()
-        if random_clouds > .5:
-            random_color = f'gray{random.randint(25,51)}'
-            random_x = random.randint(200, 800)
-            random_y = random.randint(250, 500)
-            draw_oval(canvas, random_x, random_y, random_x -
-                      150, random_y-50, fill=random_color)
-
+def draw_moon(canvas):
     for i in range(18):
         colors = f'gray{71-i}'
         draw_oval(canvas, 150+i, 500+i, 0-i, 350 -
@@ -60,10 +53,18 @@ def draw_sky(canvas, scene_width, scene_height):
                       10, random_y-10, fill=random_color)
 
 
-def draw_ground(canvas, scene_width, scene_height):
-    """Draw the ground and all the objects on the ground."""
-    draw_rectangle(canvas, 0, 0,
-                   scene_width, scene_height / 3, width=0, fill="saddleBrown")
+def draw_clouds(canvas):
+    for i in range(20):
+        random_clouds = random.random()
+        if random_clouds > .5:
+            random_color = f'gray{random.randint(25,51)}'
+            random_x = random.randint(200, 800)
+            random_y = random.randint(250, 500)
+            draw_oval(canvas, random_x, random_y, random_x -
+                      150, random_y-50, fill=random_color)
+
+
+def draw_stars(canvas):
     for i in range(50):
         random_star = random.random()
         if random_star > .5:
@@ -72,21 +73,63 @@ def draw_ground(canvas, scene_width, scene_height):
             draw_polygon(canvas, random_x, random_y, random_x+5, random_y-5,
                          random_x, random_y-10, random_x-5, random_y-5, fill='gold1')
 
-    # draw_polygon(canvas, 150, 150, 200, 100, 150, 50, 100, 100, fill='black')
+
+def draw_pine_tree(canvas):
+    """Draw a single pine tree.
+    Parameters
+        canvas: The canvas where this function
+            will draw a pine tree.
+        center_x, bottom: The x and y location in pixels where
+            this function will draw the bottom of a pine tree.
+        height: The height in pixels of the pine tree that
+            this function will draw.
+    Return: nothing
+    """
+    for x in range(200):
+        # tree_center_x = 100
+        # tree_bottom = 50
+        # tree_height = 100
+        if random.random() > .5:
+            x_river = list(range(300, 450))
+            x_ground = list(range(25, 775))
+            for x in range(len(x_river)):
+                x_ground.remove(x_river[x])
+            # center_x = random.randint(min(x_ground), max(x_ground))
+            center_x = random.choice(x_ground)
+            bottom = random.randint(0, 150)
+            height = 100
+
+            trunk_width = height / 10
+            trunk_height = height / 8
+            trunk_left = center_x - trunk_width / 2
+            trunk_right = center_x + trunk_width / 2
+            trunk_top = bottom + trunk_height
+
+            # Draw the trunk of the pine tree.
+            draw_rectangle(canvas,
+                           trunk_left, trunk_top, trunk_right, bottom,
+                           outline="gray20", width=1, fill="tan3")
+
+            skirt_width = height / 2
+            skirt_height = height - trunk_height
+            skirt_left = center_x - skirt_width / 2
+            skirt_right = center_x + skirt_width / 2
+            skirt_top = bottom + height
+
+            # Draw the crown (also called skirt) of the pine tree.
+            draw_polygon(canvas, center_x, skirt_top,
+                         skirt_right, trunk_top,
+                         skirt_left, trunk_top,
+                         outline="gray20", width=1, fill="dark green")
 
 
-def draw_grid(canvas, width, height, interval, color="snow1"):
-    # Draw a vertical line at every x interval.
-    label_y = 15
-    for x in range(interval, width, interval):
-        draw_line(canvas, x, 0, x, height, fill=color)
-        draw_text(canvas, x, label_y, f"{x}", fill=color)
-
-    # Draw a horizontal line at every y interval.
-    label_x = 15
-    for y in range(interval, height, interval):
-        draw_line(canvas, 0, y, width, y, fill=color)
-        draw_text(canvas, label_x, y, f"{y}", fill=color)
+def draw_ground(canvas, scene_width, scene_height):
+    """Draw the ground and all the objects on the ground."""
+    draw_rectangle(canvas, 0, 0,
+                   scene_width, scene_height / 3, width=0, fill="saddleBrown")
+    draw_polygon(canvas, 300, 170, 400, 170, 450,
+                 0, 350, 0, fill="steelBlue1")
+    draw_pine_tree(canvas)
 
 
 # Call the main function so that
